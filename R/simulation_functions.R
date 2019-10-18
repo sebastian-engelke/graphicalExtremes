97e4aeb91c1c51599e56827d8307975dae6b8999
Nicola <nicola.gnecco@gmail.com>
2019-10-18 17:15:44 +0200

    changes

--- R/simulation_functions.R
+++ R/simulation_functions.R
@@ -93,3 +93,126 @@ rmpareto <- function(no.simu, model, d, par) {
   return(list(res=res[sample(1:nrow(res), no.simu, replace=FALSE),],
               counter=counter))
 }
+
+
+### This function simulates tree graphical models, either multivariate Pareto or max-stable distributions
+#tree: a graph object that must be a tree
+#model: either "HR" or "logistic" for HR or logistics tree model, respectively
+#method: either "mpareto" or "maxstable"
+#loc, scale, shape: if method="maxstable", output is transformed to general GEV margins
+#no.simu: number of simulations
+#Gamma: parameter matrix if model="HR"
+#theta: parameter if model="logsitic"
+simu_tree <- function(tree, model, method, no.simu=1, Gamma=NULL, theta=NULL, alpha.mat=NULL, loc=1, scale=1, shape=1) {
+  require("igraph")
+  adj =  as.matrix(as_adj(tree))
+  d <- nrow(adj)
+  e <- ecount(tree)
+  ends.mat = ends(tree, E(tree))
+
+  stopifnot(model %in% c("logistic", "HR", "dirichlet"))
+  stopifnot((d==round(d)) & (d>=1))
+  stopifnot((no.simu==round(no.simu)) & (no.simu>=1))
+
+  if (length(loc)  ==1) loc   <- rep(loc  , times=d)
+  if (length(scale)==1) scale <- rep(scale, times=d)
+  if (length(shape)==1) shape <- rep(shape, times=d)
+  stopifnot(all(scale>1e-12))
+
+  if (model=="logistic") {
+    stopifnot(1e-12 < theta & theta < 1 - 1e-12)
+  } else if (model=="HR") {
+    par.vec = Gamma[ends.mat]
+  } else if (model=="dirichlet") {
+    stopifnot(nrow(alpha.mat) == d-1 & ncol(alpha.mat) == 2)
+  }
+
+  ## Define a matrix A[[k]] choosing the paths from k to other vertices
+  idx.e <- matrix(0, nrow=d, ncol=d)
+  idx.e[ends.mat] = 1:e
+  idx.e = idx.e + t(idx.e)
+
+  A <- e.start <- e.end <- list() #e.start[[k]][h] gives the index (1 or 2) of the starting node in the h edge in the tree rooted at k
+
+  for (k in 1:d) {
+    A[[k]] <- matrix(0, nrow=d, ncol=e)
+    e.start[[k]] = e.end[[k]] = numeric(e)
+    short.paths <- shortest_paths(tree, from = k, to=1:d)
+    for(h in 1:d){
+      path = short.paths$vpath[[h]]
+      idx.tmp = idx.e[cbind(path[-length(path)], path[-1])]
+      A[[k]][h,idx.tmp] <- 1
+      e.start[[k]][idx.tmp] = apply(ends.mat[idx.tmp,] == matrix(path[-length(path)], nrow = length(idx.tmp), ncol=2), MARGIN=1, FUN = function(x) which(x==TRUE)) #path[-length(path)]
+      e.end[[k]][idx.tmp] = apply(ends.mat[idx.tmp,] == matrix(path[-1], nrow = length(idx.tmp), ncol=2), MARGIN=1, FUN = function(x) which(x==TRUE))  #path[-1]
+    }
+  }
+
+  if(method=="mpareto")
+  {
+    counter <- 0
+    res <- numeric(0)
+    n.total <- 0
+    while (n.total < no.simu) {
+      counter <- counter + 1
+      shift <- sample(1:d, no.simu, replace=TRUE)
+      for(k in 1:d){
+        n.k <- sum(shift==k)
+        if(n.k>0){
+          proc <- switch(model,
+                         "HR" = simu_px_tree_HR(no.simu=n.k, G.vec=par.vec, A = A[[k]]),
+                         "logistic"     = simu_px_tree_logistic(no.simu=n.k, idx=k, nb.edges=e, theta=theta, A=A),
+                         "dirichlet"     = simu_px_tree_dirichlet(no.simu=n.k, alpha.start = alpha.mat[cbind(1:e, e.start[[k]])],
+                                                                  alpha.end = alpha.mat[cbind(1:e, e.end[[k]])], A=A[[k]])
+          )
+          stopifnot(dim(proc)==c(n.k, d))
+          proc <- proc/rowSums(proc) / (1-runif(nrow(proc)))
+          idx.sim <- which(apply(proc,1,max) > 1)
+          res <- rbind(res, proc[idx.sim,])
+          n.total <- nrow(res)
+        }
+      }
+    }
+  }else if(method=="maxstable"){
+    counter <- rep(0, times=no.simu)
+    res <- matrix(0, nrow=no.simu, ncol=d)
+    for (k in 1:d) {
+      poisson <- rexp(no.simu)
+
+      while (any(1/poisson > res[,k])) {
+        ind <- (1/poisson > res[,k])
+        n.ind <- sum(ind)
+        idx <- (1:no.simu)[ind]
+        counter[ind] <- counter[ind] + 1
+        proc <- switch(model,
+                       "HR" = simu_px_tree_HR(no.simu=n.ind, G.vec=par.vec, A = A[[k]]),
+                       "logistic"     = simu_px_tree_logistic(no.simu=n.ind, idx=k, nb.edges=e, theta=theta, A=A),
+                       "dirichlet"     = simu_px_tree_dirichlet(no.simu=n.ind, alpha.start = alpha.mat[cbind(1:e, e.start[[k]])],
+                                                                alpha.end = alpha.mat[cbind(1:e, e.end[[k]])], A=A[[k]])
+        )
+        stopifnot(dim(proc)==c(n.ind, d))
+        if (k==1) {
+          ind.upd <- rep(TRUE, times=n.ind)
+        } else {
+          ind.upd <- sapply(1:n.ind, function(i)
+            all(1/poisson[idx[i]]*proc[i,1:(k-1)] <= res[idx[i],1:(k-1)]))
+        }
+        if (any(ind.upd)) {
+          idx.upd <- idx[ind.upd]
+          res[idx.upd,] <- pmax(res[idx.upd,], 1/poisson[idx.upd]*proc[ind.upd,])
+        }
+        poisson[ind] <- poisson[ind] + rexp(n.ind)
+      }
+    }
+    res <- sapply(1:d, function(i) {
+      if (abs(shape[i]<1e-12)) {
+        return(log(res[,i])*scale[i] + loc[i])
+      } else {
+        return(1/shape[i]*(res[,i]^shape[i]-1)*scale[i] + loc[i])
+      }
+    })
+
+  }
+  return(list(res=res[sample(1:nrow(res), no.simu, replace=FALSE),], counter=counter))
+}
+
+
--- R/simulation_helpers.R
+++ R/simulation_helpers.R
@@ -3,11 +3,89 @@
 #' Simulate the Huessler-Reiss extremal functions
 #'
 #' @param no.simu Integer. Number of simulations, by default equal to 1
-#' @param idx Integer.
-simu_px_HR <- function(no.simu=1, idx, trend, chol.mat) {
+#' @param idx Integer. Related index corresponding to the variable  over which variable we use  extremal function is simulated.
+#' @param trend
+simu_px_HR <- function(no.simu=1, idx, trend, chol.mat, d) {
   stopifnot(length(idx)==1)
   d <- nrow(chol.mat)
   proc <- t(chol.mat)%*%matrix(rnorm(d*no.simu), ncol=no.simu) - trend
   proc <- exp(t(proc) - proc[idx,])
   return(proc)
 }
+
+
+
+### Internal: simulates logistic extremal functions
+simu_px_logistic <- function(no.simu=1, idx, N, theta) {
+  stopifnot(length(idx)==1 || length(idx)==no.simu)
+  res       <- matrix(1/gamma(1-theta)*(-log(runif(no.simu*N)))^(-theta), nrow=no.simu, ncol=N)
+  res[cbind(1:no.simu,idx)] <- 1/gamma(1-theta)*rgamma(no.simu,shape=1-theta)^(-theta)
+  return(res/res[cbind(1:no.simu,idx)])
+}
+
+
+### Internal: simulates negative logistic extremal functions
+simu_px_neglogistic <- function(no.simu=1, idx, N, theta) {
+  stopifnot(length(idx)==1 || length(idx)==no.simu)
+  res       <- matrix(rweibull(no.simu*N, shape=theta, scale=1/gamma(1+1/theta)), nrow=no.simu, ncol=N)
+  res[cbind(1:no.simu,idx)] <- 1/gamma(1+1/theta)*rgamma(no.simu,shape=1+1/theta)^(1/theta)
+  return(res/res[cbind(1:no.simu,idx)])
+}
+
+
+### Internal: simulates Dirichlet extremal functions
+simu_px_dirichlet <- function(no.simu, idx, N, alpha) {
+  stopifnot(length(idx)==1 || length(idx)==no.simu)
+  shape <- alpha
+  shape[idx] <- alpha[idx] + 1
+  shape.mat <- matrix(shape, nrow=N, ncol=no.simu)
+  rate.mat <- matrix(alpha, nrow=N, ncol=no.simu)
+  res <- t(matrix(rgamma(N*no.simu, shape=shape.mat, rate=rate.mat), nrow=N, ncol=no.simu))
+  return(res/res[cbind(1:no.simu,idx)])
+}
+
+
+### Internal: simulates Dirichlet mixture extremal functions
+simu_px_dirichlet_mix <- function(no.simu, idx, N, weights, alpha, norm.alpha) {
+  stopifnot(length(idx)==1 || length(idx)==no.simu)
+  if (length(idx)==1) {
+    k <- sample(1:length(weights), no.simu, replace=TRUE, prob=N*weights*norm.alpha[idx,])
+  } else {
+    k <- sapply(1:no.simu, function(i) sample(1:length(weights), 1, prob=N*weights*norm.alpha[idx[i],]))
+  }
+  shape.mat <- alpha[,k,drop=FALSE]
+  shape.mat[cbind(idx,1:no.simu)] <- shape.mat[cbind(idx,1:no.simu)]+1
+  res <- t(matrix(rgamma(N*no.simu, shape=shape.mat), nrow=N, ncol=no.simu))
+  return(res/res[cbind(1:no.simu,idx)])
+}
+
+
+### Internal: simulates HR extremal functions on a tree
+simu_px_tree_HR <- function(no.simu=1, G.vec, A) {
+  res <- exp(A %*% matrix(rnorm(length(G.vec)*no.simu, mean= -G.vec/2, sd=sqrt(G.vec)), ncol=no.simu))
+  return(t(res))
+}
+
+### Internal: simulates logistic extremal functions on a tree
+simu_px_tree_logistic <- function(no.simu=1, idx, nb.edges, theta, A) {
+  stopifnot(length(idx)==1 || length(idx)==no.simu)
+  res       <- exp(A[[idx]] %*% log(matrix(1/gamma(1-theta)*(-log(runif(no.simu*nb.edges)))^(-theta) /
+                                             (1/gamma(1-theta)*rgamma(no.simu*nb.edges,shape=1-theta)^(-theta)), ncol=no.simu)))
+  return(t(res))
+}
+
+### Internal: simulates Dirichlet extremal functions on a tree
+simu_px_tree_dirichlet <- function(no.simu=1, alpha.start, alpha.end, A) {
+  e = length(alpha.start)
+  shape.start = matrix(alpha.start + 1, nrow=e, ncol=no.simu)
+  rate.start = matrix(alpha.start, nrow=e, ncol=no.simu)
+  shape.end = matrix(alpha.end, nrow=e, ncol=no.simu)
+  rate.end = matrix(alpha.end, nrow=e, ncol=no.simu)
+  sim.start = matrix(rgamma(e*no.simu, shape=shape.start, rate=rate.start), nrow=e, ncol=no.simu)
+  sim.end = matrix(rgamma(e*no.simu, shape=shape.end, rate=rate.end), nrow=e, ncol=no.simu)
+  res       <- exp(A %*% log(sim.end / sim.start))
+  return(t(res))
+}
+
+
+
