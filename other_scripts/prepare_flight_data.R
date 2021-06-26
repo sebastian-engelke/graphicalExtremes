080751ff133f17cd93d523a7957d00ad9dd3eeaf
Nicola Gnecco <nicola.gnecco@gmail.com>
2021-06-26 09:41:52 +0200

    Flight data

--- other_scripts/prepare_flight_data.R
+++ other_scripts/prepare_flight_data.R
@@ -3,6 +3,7 @@ library(dplyr)
 flights <- list()
 flights$data <- mat
 flights$airports <- selected_airports
-flights$connections <- flights_connections
+flights$connections <- flights_connections %>%
+  select(-ids.x, -ids.y)
 
 usethis::use_data(flights, overwrite = TRUE)
