d8b1e3be63d836fa5fc837ef6f36d02b3c2d3dfb
Manuel Hentschel <53863351+ManuelHentschel@users.noreply.github.com>
2021-12-10 14:27:06 +0100

    Remove golazo dependency

--- R/emtp2.R
+++ R/emtp2.R
@@ -1,13 +1,4 @@
 
-##Functions+settings for emtp2
-settings <- osqp::osqpSettings(verbose = FALSE, eps_abs = 1e-10, eps_rel = 1e-10)
-
-#' @export
-start.positive.dual <- function(S){
-  Z <- golazo::Zmatrix(S)
-  return(Sigma2Gamma(Z))
-}
-
 
 #' Performs Gaussian likelihood optimization under Laplacian matrix constraints.
 #'
@@ -29,13 +20,18 @@ emtp2 <- function(G,tol=1e-6, verbose = TRUE, initial_point = TRUE){
   Gam <- G
   if (initial_point==TRUE){
     P <- diag(d)-matrix(1,d,d)/(d)
-    Gam <- start.positive.dual(P%*%(-G/2)%*%P)
+    S <- P%*%(-G/2)%*%P
+    Z <- Zmatrix(S)
+    Gam <- Sigma2Gamma(Z)
   }
   it <- 0
   if (verbose==TRUE){
     cat("\n The algorithm will stop when the duality gap is below: ",tol,"\b.\n\n")
     cat("Iteration | Gap\n")
   }
+
+  settings <- osqp::osqpSettings(verbose = FALSE, eps_abs = 1e-10, eps_rel = 1e-10)
+
   gap <- Inf
   while (gap>tol){
     Gam0 <- Gam
@@ -54,6 +50,47 @@ emtp2 <- function(G,tol=1e-6, verbose = TRUE, initial_point = TRUE){
     }
   }
   return(list(G_emtp2=Gam,it=it))
+}
+
+
+
+
+#' Computes the Z-matrix
+#'
+#' Copied from the R package "golazo" with kind permission by Piotr Zwiernik <piotr.zwiernik@upf.edu>.
+#' This function outputs the Z matrix, that is, the unique ultrametric matrix dominating S. This matrix is used to connstruct a starting point in teh GOLAZO algorithm when L=0 but U has strictly positive (off-diagonal entries).
+#' @param S a covariance matrix
+#' @keywords Z-matrix, starting point
+Zmatrix <- function(S){
+  p <- nrow(S)
+  R <- stats::cov2cor(S)
+  # Compute the distances. Non-positive correlations correspond to very big distances.
+  D <- stats::as.dist(-log((R>0)*R+(R<=0)*1e-20))
+  # use single-linkage clustering method in R
+  hcl <- stats::hclust(D,method="single")
+  # recover how hclust merges variables and use it to recover the corresponding ultrametric
+  subs <- list()
+  length(subs) <- p-1
+  Z <- matrix(0,p,p)
+    for (i in 1:(p-1)){
+      if (hcl$merge[i,1]<0 && hcl$merge[i,2]<0) {
+        subs[[i]] <- union(-hcl$merge[i,1],-hcl$merge[i,2])
+        Z[-hcl$merge[i,1],-hcl$merge[i,2]]<- Z[-hcl$merge[i,2],-hcl$merge[i,1]] <- hcl$height[i]}
+      else if (hcl$merge[i,1]<0 && hcl$merge[i,2]>0) {
+        subs[[i]] <- union(-hcl$merge[i,1],subs[[hcl$merge[i,2]]])
+        Z[-hcl$merge[i,1],subs[[hcl$merge[i,2]]]] <- hcl$height[i]
+        Z[subs[[hcl$merge[i,2]]],-hcl$merge[i,1]] <- hcl$height[i]
+      }
+      else {
+        subs[[i]] <- union(subs[[hcl$merge[i,1]]],subs[[hcl$merge[i,2]]])
+        Z[subs[[hcl$merge[i,1]]],subs[[hcl$merge[i,2]]]] <- hcl$height[i]
+        Z[subs[[hcl$merge[i,2]]],subs[[hcl$merge[i,1]]]] <- hcl$height[i]
+          }
+    }
+  # Z is the corresponding ultrametric. Now compute the indiced correlation matrix
+  Z <- exp(-Z)
+  # finally output the corresponding covariance matrix
+  return(diag(sqrt(diag(S)))%*%Z%*%diag(sqrt(diag(S))))
 }
 
 
