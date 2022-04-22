2bb4f85b2329556492115a4d1a8a88159067c89d
Manuel Hentschel <53863351+ManuelHentschel@users.noreply.github.com>
2022-04-22 14:15:24 +0200

    changes

--- R/autoregressive.R
+++ R/autoregressive.R
@@ -16,22 +16,14 @@ ar_gauss <- function(Sigma, mu=NULL, graph=NULL, tol=1e-6){
   if(is.null(mu)){
     mu <- numeric(d)
   }
-  
+
   # Make separators etc.
   cliques <- order_cliques(igraph::maximal.cliques(graph))
-  tmp <- c()
-  JList <- list()
-  for(i in seq_along(cliques)){
-    tmp <- union(tmp, cliques[[i]])
-    JList[[i]] <- tmp
-  }
-  separators <- lapply(seq_len(length(cliques)-1)+1, function(i){
-    intersect(cliques[[i]], JList[[i-1]])
-  })
-  EList <- lapply(seq_len(length(cliques)-1)+1, function(i){
-    setdiff(cliques[[i]], JList[[i-1]])
-  })
-  
+  tmp <- computeVertexSets(cliques)
+  separators <- tmp$separators
+  JList <- tmp$JList
+  EList <- tmp$EList
+
   # Compute vectors/matrices
   cli1 <- cliques[[1]]
   mu1 <- mu[cli1]
@@ -56,6 +48,63 @@ ar_gauss <- function(Sigma, mu=NULL, graph=NULL, tol=1e-6){
     SigmaList = SigmaList,
     MList = MList,
     muList = muList
+  ))
+}
+
+ar2Sigma_gauss <- function(cliques, mu1, Sigma1, SigmaList, MList, muList){
+  d <- max(do.call(`c`, cliques))
+  Sigma <- matrix(0, d, d)
+  mu <- numeric(d)
+  
+  # Compute separators etc.
+  tmp <- computeVertexSets(cliques)
+  separators <- tmp$separators
+  JList <- tmp$JList
+  EList <- tmp$EList
+
+  # Reconstruct Sigma, mu
+  cli1 <- cliques[[1]]
+  mu[cli1] <- mu1
+  Sigma[cli1, cli1] <- Sigma1
+  
+  for(i in seq_len(length(cliques) - 1)){
+    D <- separators[[i]]
+    E <- EList[[i]]
+    J <- JList[[i]]
+    M <- MList[[i]]
+    mu[E] <- muList[[i]] + M %*% mu[D]
+    Sigma[E, E] <- M %*% Sigma[D, D] %*% t(M) + SigmaList[[i]]
+    Sigma[J, E] <- Sigma[J, D] %*% t(M)
+    Sigma[E, J] <- t(Sigma[J, E])
+  }
+  
+  return(list(
+    mu = mu,
+    Sigma = Sigma
+  ))
+}
+
+
+
+computeVertexSets <- function(cliques){
+  # Make separators etc.
+  tmp <- c()
+  JList <- list()
+  for(i in seq_along(cliques)){
+    tmp <- union(tmp, cliques[[i]])
+    JList[[i]] <- tmp
+  }
+  separators <- lapply(seq_len(length(cliques)-1)+1, function(i){
+    intersect(cliques[[i]], JList[[i-1]])
+  })
+  EList <- lapply(seq_len(length(cliques)-1)+1, function(i){
+    setdiff(cliques[[i]], JList[[i-1]])
+  })
+  return(list(
+    cliques = cliques,
+    separators = separators,
+    JList = JList,
+    EList = EList
   ))
 }
 
