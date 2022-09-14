356b177c84c8902743d9908821a911cad3948aa3
IanFla <51277149+IanFla@users.noreply.github.com>
2022-09-14 10:33:23 +0800

    Update generate_examples.R

--- R/generate_examples.R
+++ R/generate_examples.R
@@ -119,7 +119,7 @@ generate_random_integer_Gamma <- function(d, b=2, b_step=1){
 generate_random_spd_matrix <- function(d, bMin=-10, bMax=10, ...){
   B <- matrix(bMin + stats::runif(d**2) * (bMax-bMin), d, d)
   M <- B %*% t(B)
-  while(!matrixcalc::is.symmetric.matrix(M)) {
+  while(!isSymmetric.matrix(M)) {
     B <- matrix(bMin + stats::runif(d**2) * (bMax-bMin), d, d)
     M <- B %*% t(B)
   }
