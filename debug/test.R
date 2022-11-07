4672d9c963cb2a4f9b5c6de959af52e159fbf0dc
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2022-11-07 19:02:06 +0100

    Changes

--- debug/test.R
+++ debug/test.R
@@ -1,5 +1,5 @@
 
-# devtools::load_all('.')
+devtools::load_all('.')
 library(igraph)
 library(tictoc)
 
@@ -13,8 +13,10 @@ d <- 5
 n <- 100
 
 # g <- generate_random_connected_graph(d, p = 3/(d+1))
-g <- generate_random_connected_graph(d)
-G0 <- ensure_symmetry(generate_random_Gamma(d), Inf)
+# g <- generate_random_connected_graph(d)
+g <- igraph::make_ring(d)
+G0 <- ensure_symmetry(generate_random_graphical_Gamma(g))
+# G0 <- ensure_symmetry(generate_random_Gamma(d), Inf)
 par <- G0
 
 
@@ -22,9 +24,9 @@ data <- rmpareto(n, 'HR', d, par)
 
 init <- upper.tri.val(emp_vario(data))
 
-cat('MLE Gamma...\n')
-par2 <- fmpareto_HR_MLE_Gamma(data)
+# cat('MLE Gamma...\n')
+# par2 <- fmpareto_HR_MLE_Gamma(data, graph = g)
 
 cat('MLE Theta...\n')
-par3 <- fmpareto_HR_MLE_Theta(data)
+par3 <- fmpareto_HR_MLE_Theta(data, graph = g)
 
