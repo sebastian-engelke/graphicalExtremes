e782838e0c6fec699c0f4be843b4a2cd5b7e371b
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2022-11-17 16:45:44 +0100

    Changes

--- R/data.R
+++ R/data.R
@@ -7,11 +7,14 @@
 "danube"
 
 
-#' Flights delay data.
+#' DEPRECATED: Flights delay data.
 #'
 #' A dataset containing daily total delays (Jan 1, 2015 -- Dec 31, 2015)
 #' of Southwest Airlines at different airports in California, Nevada,
 #' Arizona, Utah, and Texas.
+#' 
+#' This dataset was called `flights` in earlier development versions of this package
+#' and is is superseded by the new dataset now called `flights`.
 #'
 #' @source U.S. Department of Transportation's (DOT) Bureau of Transportation Statistics <https://www.bts.gov/>.
-"flights"
+"flights_old"
