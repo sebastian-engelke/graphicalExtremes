a7a9b03276f98e8d797962b5bf4c477a70cfd584
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2022-11-22 12:08:11 +0100

    Changes

--- debug/test.R
+++ debug/test.R
@@ -1,31 +1,50 @@
 
+## Try to figure out what the coordinates in danube data are
+
 if(!nchar(Sys.getenv('VSCODE_DEBUG_SESSION'))){
     devtools::load_all('.')
 }
 # library(igraph)
 # library(tictoc)
-
-load('debug/graph.Rdata')
-load('debug/variogram.Rdata')
-
-# loads:
-vario_emp
-vario_emp <- ensure_symmetry(vario_emp)
-graph
+library(implicitExpansion)
 
 
-G1 <- complete_Gamma_general_sc(vario_emp, graph, N=100000)
+g <- igraph::graph_from_edgelist(danube$flow_edges)
 
-T1 <- Gamma2Theta(G1)
+loc <- danube$coords_to_plot
 
-# Check Theta-zeros in non-edge entries:
-max(abs(getNonEdgeEntries(T1, graph)))
+loc <- as.matrix(danube$info[,c('Long', 'Lat')])
 
-# Check unchanged Gamma in edge entries:
-max(abs(getEdgeEntries(G1 - vario_emp, graph)))
+loc1 <- as.matrix(danube$info[,c('Long', 'Lat')])
+loc2 <- as.matrix(danube$info[,c('Long_Center', 'Lat_Center')])
 
-# Check that G1 is valid:
-is_sym_cnd(G1)
+locAll <- rbind(loc1, loc2)
 
+xLim <- range(locAll[,1])
+yLim <- range(locAll[,2])
 
+edgeData <- lapply(seq_len(nrow(danube$flow_edges)), function(i){
+    edge <- danube$flow_edges[i,]
+    loc1[c(edge, NA),]
+})
+locEdges <- do.call(rbind, edgeData)
 
+lineData <- lapply(seq_len(nrow(loc1)), function(i){
+    rbind(
+        loc1[i,],
+        loc2[i,],
+        NA
+    )
+})
+locLines <- do.call(rbind, lineData)
+
+plot(loc1, xlim = xLim, ylim = yLim)
+lines(locEdges)
+points(loc2, col='red')
+lines(locLines, col='red', lty=2)
+
+# dev.new()
+plot(g, layout=loc1)
+
+rn <- rownames(danube$data)
+colSums(rn %m==% t(unique(rn)))
