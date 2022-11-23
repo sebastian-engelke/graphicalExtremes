a1f6a97c6d79db68cea4dd68a1bb46e7b2a7daf1
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2022-11-23 15:45:25 +0100

    Changes

--- R/flight_plotting.R
+++ R/flight_plotting.R
@@ -1,26 +1,231 @@
 
-# library(dplyr)
-# library(ggplot2)
-# library(magrittr)
 
-source('plotThinning.R')
+#' @export
+plotFlights <- function(
+  airportIndices = NULL,
+  airports_sel = NULL,
+  connections_sel = NULL,
+  useAirportNFlights = FALSE,
+  useConenctionNFlights = FALSE,
+  minNFlights = 0,
+  graph = NULL,
+  map = 'state',
+  plotAirports = TRUE,
+  plotConnections = TRUE,
+  returnGGPlot = FALSE,
+  vertexColors = NULL,
+  vertexShapes = NULL,
+  xyRatio = NULL,
+  clipMap = FALSE,
+  useLatex = FALSE,
+  edgeAlpha = 0.2
+) {
 
-if(is.null(globalenv()[['DO_PLOT']])){
-  DO_PLOT <- TRUE
+  # Use all airports, conenctions if not specified
+  if(is.null(airports_sel)){
+    airports_sel <- flights$airports
+  }
+
+  # Use airportIndices from graph, if not specified:
+  if(!is.null(graph)){
+    vNames <- igraph::V(graph)$name
+    if(!is.null(vNames) && is.null(airportIndices)){
+      airportIndices <- vNames
+    }
+  }
+  
+  # Set map to NULL if not specified:
+  if(is.null(map) || is.na(map) || nchar(map) == 0){
+    map <- NULL
+  }
+
+  # Make selection of airports:
+  if(!is.null(airportIndices)){
+    airports_sel <- airports_sel[airportIndices,]
+  }
+  IATAS <- airports_sel[,'IATA']
+  
+  # If necessary, compute connections and flight counts
+  computeConnections <- is.null(connections_sel) && plotConnections
+  computeNFlights <- is.null(airports_sel$nFlights) && useAirportNFlights && plotAirports
+  if(computeConnections || computeNFlights){
+    # used multiple times below
+    nFlightMat <- apply(flights$flightCounts[IATAS,IATAS,], c(1,2), sum)
+  }
+  # Make connections list if not given
+  if(computeConnections){
+    connections_sel <- nFlightMatrixToConnectionList(nFlightMat)
+  }
+  # Compute flight counts per airport, if necessary
+  if(computeNFlights){
+    airports_sel$nFlights <- rowSums(nFlightMat) + colSums(nFlightMat) # add arriving and departing flights
+  }
+  
+  if(!is.null(vertexColors)){
+    airports_sel[,'vertexColor'] <- vertexColors[IATAS]
+    aesVertexColor <- 'vertexColor'
+  } else{
+    airports_sel[,'vertexColor'] <- NA
+    aesVertexColor <- NULL
+  }
+  
+  if(!is.null(vertexShapes)){
+    airports_sel[,'vertexShape'] <- as.character(vertexShapes[IATAS])
+    aesVertexShape <- 'vertexShape'
+  } else{
+    airports_sel[,'vertexShape'] <- NA
+    aesVertexShape <- NULL
+  }
+
+  # Make sure number of graph vertices and selected airports match:
+  if(!is.null(graph)){
+    nVertices <- igraph::vcount(graph)
+    nAirports <- nrow(airports_sel)
+    if(nVertices != nAirports){
+      stop(sprintf(
+        'Number of vertices (%d) and number of selected airports (%d) are different!',
+        nVertices,
+        nAirports
+      ))
+    }
+  }
+
+  # Prepare connections plotting:
+  if(plotConnections){
+    # Make selection of connections:
+    if(is.null(graph)){
+      # Select all connections, that are between selected airports
+      ind <- (
+        connections_sel$departureAirport %in% IATAS
+        | connections_sel$arrivalAirport %in% IATAS
+      )
+      connections_sel <- connections_sel[ind,]
+    } else{
+      # Convert graph to connections list
+      m <- igraph::get.edgelist(graph, names=FALSE)
+      connections_graph <- data.frame(matrix(IATAS[m], ncol = 2))
+      airportColNames <- c('departureAirport', 'arrivalAirport')
+      colnames(connections_graph) <- airportColNames
+
+      # Read nFlights per connection from connections_sel
+      if(useConenctionNFlights){
+        connections_graph$nFlights <- 0
+        for(i in seq_len(nrow(connections_graph))){
+          ap1 <- connections_graph$departureAirport[i]
+          ap2 <- connections_graph$arrivalAirport[i]
+          connections_graph$nFlights[i] <- sum(connections_sel$nFlights[
+            connections_sel$departureAirport %in% c(ap1, ap2)
+            & connections_sel$arrivalAirport %in% c(ap1, ap2)
+          ], na.rm = TRUE)
+        }
+      }
+    }
+
+    # Add coordinates to selected connections:
+    connections_sel$x0 <- airports_sel[connections_sel$departureAirport, 'Longitude']
+    connections_sel$y0 <- airports_sel[connections_sel$departureAirport, 'Latitude']
+    connections_sel$x1 <- airports_sel[connections_sel$arrivalAirport, 'Longitude']
+    connections_sel$y1 <- airports_sel[connections_sel$arrivalAirport, 'Latitude']
+
+    # Filter out connections with <minNFlights flights:
+    connections_sel <- connections_sel[connections_sel$nFlights >= minNFlights,]
+  }
+  
+  # Specify whether to size vertices/edges by nFlights:
+  aesSizeNodes <- NULL
+  aesSizeEdges <- NULL
+  if(useAirportNFlights){
+    aesSizeNodes <- 'nFlights'
+  }
+  if(useConenctionNFlights){
+    aesSizeEdges <- 'nFlights'
+  }
+
+  # Main plot object:
+  ggp <- (
+    ggplot2::ggplot()
+    + ggplot2::xlab(NULL)
+    + ggplot2::ylab(NULL)
+    + ggplot2::scale_x_continuous(labels = function(x) formatDegrees(x, 'EW', useLatex))
+    + ggplot2::scale_y_continuous(labels = function(x) formatDegrees(x, 'NS', useLatex))
+  )
+  
+  # Plot US map in background:
+  if(!is.null(map)){
+    dmap <- ggplot2::map_data(map)
+    ggp <- ggp + ggplot2::geom_polygon(
+      data = dmap,
+      ggplot2::aes(x = long, y = lat, group = group),
+      color = "grey65",
+      fill = "#f9f9f9",
+      size = 0.2
+    )
+  }
+  
+  # Manually set axes limits (clips map, sets aspect ratio):
+  # TODO: consider different crs from
+  # https://ggplot2.tidyverse.org/reference/ggsf.html
+  if(!is.null(xyRatio) || (clipMap && !is.null(map))){
+    xData <- airports_sel$Longitude
+    yData <- airports_sel$Latitude
+    if(!clipMap && !is.null(map)){
+      m <- map_data(map)
+      xData <- c(xData, range(m$long))
+      yData <- c(yData, range(m$lat))
+    }
+    limits <- computeLimits(
+      xData,
+      yData,
+      xyRatio = xyRatio
+    )
+    ggp <- ggp + ggplot2::coord_cartesian(
+      xlim = limits$xlim,
+      ylim = limits$ylim
+    ) + theme(
+      aspect.ratio = limits$xyRatio
+    )
+  }
+
+  # Plot airports:
+  if(plotAirports){
+    ggp <- ggp + ggplot2::geom_point(
+      data = airports_sel,
+      ggplot2::aes_string(
+        x = 'Longitude',
+        y = 'Latitude',
+        size = aesSizeNodes,
+        col = aesVertexColor,
+        shape = aesVertexShape
+      ),
+      na.rm = TRUE,
+      alpha = 1
+    )
+  }
+  
+  # Plot connections:
+  if(plotConnections){
+    ggp <- ggp + ggplot2::geom_segment(
+      data = connections_sel,
+      ggplot2::aes_string(
+        x = 'x0',
+        xend = 'x1',
+        y = 'y0',
+        yend = 'y1',
+        size = aesSizeEdges
+      ),
+      alpha = edgeAlpha
+    )
+  }
+  
+  # Return ggplot object if specified:
+  if(returnGGPlot){
+    return(ggp)
+  }
+
+  # Call plot:
+  plot(ggp)
+  return(invisible(NULL))
 }
-
-### ONLY WORKS WHEN SOURCED AFTER/FROM `prep.R`
-airports <- airports
-connections <- connections
-nDays <- length(dates)
-
-theme_set(theme_bw() +
-            theme(plot.background = element_blank(),
-                  legend.background = element_blank(),
-                  strip.background = element_rect(fill = "white")))
-
-my_palette <- c("#D55E00", "#0072B2", "#009E73", "#E69F00", "#56B4E9",
-                "#CC79A7")
 
 
 nFlightMatrixToConnectionList <- function(nFlightsPerConnection, directed=TRUE){
@@ -49,232 +254,6 @@ nFlightMatrixToConnectionList <- function(nFlightsPerConnection, directed=TRUE){
     nFlights = flightCounts
   )
   return(df)
-}
-
-
-
-# Plot flight connection
-plotFlights <- function(
-  airportIndices = NULL,
-  airports_sel = NULL,
-  connections_sel = NULL,
-  useNFlights = c(FALSE, FALSE),
-  minNFlights = 0,
-  graph = NULL,
-  map = 'state',
-  plotAirports = TRUE,
-  plotConnections = TRUE,
-  returnGGPlot = FALSE,
-  vertexColors = NULL,
-  vertexShapes = NULL,
-  xyRatio = NULL,
-  clipMap = FALSE,
-  useLatex = FALSE,
-  edgeAlpha = 0.2
-) {
-
-  # Use all airports, conenctions if not specified
-  if(is.null(airports_sel)){
-    airports_sel <- flights$airports
-  }
-  if(is.null(connections_sel)){
-    nFlightMat <- apply(flights$flightCounts, c(1,2), sum)
-    connections_sel <- nFlightMatrixToConnectionList(nFlightMat)
-  }
-
-  # Use airportIndices from graph, if not specified:
-  if(!is.null(graph)){
-    vNames <- igraph::V(graph)$name
-    if(!is.null(vNames) && is.null(airportIndices)){
-      airportIndices <- vNames
-    }
-  }
-  
-  # Set map to NULL if not specified:
-  if(is.null(map) || is.na(map) || nchar(map) == 0){
-    map <- NULL
-  }
-
-  # Make selection of airports:
-  if(!is.null(airportIndices)){
-    airports_sel <- airports_sel[airportIndices,]
-  }
-  IATAS <- airports_sel[,'IATA']
-  
-  if(!is.null(vertexColors)){
-    airports_sel[,'vertexColor'] <- vertexColors[IATAS]
-    aesVertexColor <- 'vertexColor'
-  } else{
-    airports_sel[,'vertexColor'] <- NA
-    aesVertexColor <- NULL
-  }
-  
-  if(!is.null(vertexShapes)){
-    airports_sel[,'vertexShape'] <- as.character(vertexShapes[IATAS])
-    aesVertexShape <- 'vertexShape'
-  } else{
-    airports_sel[,'vertexShape'] <- NA
-    aesVertexShape <- NULL
-  }
-  
-  airports_sel %<>% mutate(dailyFlights = nFlights / nDays)
-
-  # Make sure number of graph vertices and selected airports match:
-  if(!is.null(graph)){
-    nVertices <- igraph::vcount(graph)
-    nAirports <- nrow(airports_sel)
-    if(nVertices != nAirports){
-      stop(sprintf(
-        'Number of vertices (%d) and number of selected airports (%d) are different!',
-        nVertices,
-        nAirports
-      ))
-    }
-  }
-
-  # Prepare connections plotting:
-  if(plotConnections){
-    # Make selection of connections:
-    if(is.null(graph)){
-      connections_sel <- (
-        connections_sel
-        %>% filter(departureAirport %in% IATAS)
-        %>% filter(arrivalAirport %in% IATAS)
-        %>% mutate(dailyFlights = nFlights / nDays)
-      )
-    } else{
-      m <- igraph::get.edgelist(graph, names=FALSE)
-      connections_graph <- data.frame(matrix(IATAS[m], ncol = 2))
-      airportColNames <- c('departureAirport', 'arrivalAirport')
-      colnames(connections_graph) <- airportColNames
-      connections_sel <- (
-        left_join(connections_graph, connections_sel, by=airportColNames)
-        %>% replace_na(list(nFlights = 0))
-        %>% mutate(dailyFlights = nFlights / nDays)
-      )
-    }
-
-    # Add coordinates to selected connections:
-    connections_sel %<>% mutate(
-      x0 = airports_sel[departureAirport, 'Longitude'],
-      y0 =  airports_sel[departureAirport, 'Latitude'],
-      x1 = airports_sel[arrivalAirport, 'Longitude'],
-      y1 =  airports_sel[arrivalAirport, 'Latitude']
-    )
-
-    # Filter out connections with <minNFlights flights:
-    connections_sel %<>% filter(nFlights >= minNFlights)
-  }
-  
-  # Specify whether to size vertices/edges by nFlights:
-  if(length(useNFlights) < 2){
-    useNFlights <- c(useNFlights, useNFlights)
-  }
-  aesSizeNodes <- NULL
-  aesSizeEdges <- NULL
-  if(useNFlights[1]){
-    aesSizeNodes <- 'dailyFlights'
-  }
-  if(useNFlights[2]){
-    aesSizeEdges <- 'dailyFlights'
-  }
-
-  # Main plot object:
-  ggp <- (
-    ggplot()
-    + xlab(NULL)
-    + ylab(NULL)
-    + scale_x_continuous(labels = function(x) formatDegrees(x, 'EW', useLatex))
-    + scale_y_continuous(labels = function(x) formatDegrees(x, 'NS', useLatex))
-  )
-  
-  # Legend title
-  if(any(useNFlights)){
-    ggp <- ggp + labs(size = 'Daily Flights')
-  }
-
-  # Plot US map in background:
-  if(!is.null(map)){
-    dmap <- map_data(map)
-    ggp <- ggp + geom_polygon(
-      data = dmap,
-      aes(x = long, y = lat, group = group),
-      color = "grey65",
-      fill = "#f9f9f9",
-      size = 0.2
-    )
-  }
-  
-  # Manually set axes limits (clips map, sets aspect ratio):
-  # TODO: consider different crs from
-  # https://ggplot2.tidyverse.org/reference/ggsf.html
-  if(!is.null(xyRatio) || (clipMap && !is.null(map))){
-    xData <- airports_sel$Longitude
-    yData <- airports_sel$Latitude
-    if(!clipMap && !is.null(map)){
-      m <- map_data(map)
-      xData <- c(xData, range(m$long))
-      yData <- c(yData, range(m$lat))
-    }
-    limits <- computeLimits(
-      xData,
-      yData,
-      xyRatio = xyRatio
-    )
-    ggp <- ggp + coord_cartesian(
-      xlim = limits$xlim,
-      ylim = limits$ylim
-    ) + theme(
-      aspect.ratio = limits$xyRatio
-    )
-  }
-
-  # Plot airports:
-  if(plotAirports){
-    ggp <- ggp + geom_point(
-      data = airports_sel,
-      aes_string(
-        x = 'Longitude',
-        y = 'Latitude',
-        size = aesSizeNodes,
-        col = aesVertexColor,
-        shape = aesVertexShape
-      ),
-      na.rm = TRUE,
-      alpha = 1
-    )
-  }
-  
-  # Plot connections:
-  if(plotConnections){
-    ggp <- ggp + geom_segment(
-      data = connections_sel,
-      aes_string(
-        x = 'x0',
-        xend = 'x1',
-        y = 'y0',
-        yend = 'y1',
-        size = aesSizeEdges
-      ),
-      alpha = edgeAlpha
-    )
-  }
-  
-  # ggp <- ggp + geom_sf() + coord_sf(
-  #   label_axes = '--EN',
-  #   label_graticule = 'EN'
-  # )
-  
-  # ggp <- ggp + theme(axis.ticks.x = )
-  
-  # Return ggplot object if specified:
-  if(returnGGPlot){
-    return(ggp)
-  }
-
-  # Actual plot:
-  plot(ggp)
-  return(invisible(NULL))
 }
 
 formatDegrees <- function(decDeg, direction = 'NS', latex=TRUE){
---
+++ debug/test_danube_coords.R
@@ -0,0 +1,57 @@
+
+## Try to figure out what the coordinates in danube data are
+
+if(!nchar(Sys.getenv('VSCODE_DEBUG_SESSION'))){
+    devtools::load_all('.')
+}
+# library(igraph)
+# library(tictoc)
+library(implicitExpansion)
+
+
+g <- igraph::graph_from_edgelist(danube$flow_edges)
+
+loc <- danube$coords_to_plot
+
+loc <- as.matrix(danube$info[,c('Long', 'Lat')])
+
+loc1 <- as.matrix(danube$info[,c('Long', 'Lat')])
+loc2 <- as.matrix(danube$info[,c('Long_Center', 'Lat_Center')])
+
+locAll <- rbind(loc1, loc2)
+
+xLim <- range(locAll[,1])
+yLim <- range(locAll[,2])
+
+edgeData <- lapply(seq_len(nrow(danube$flow_edges)), function(i){
+    edge <- danube$flow_edges[i,]
+    loc1[c(edge, NA),]
+})
+locEdges <- do.call(rbind, edgeData)
+
+lineData <- lapply(seq_len(nrow(loc1)), function(i){
+    rbind(
+        loc1[i,],
+        loc2[i,],
+        NA
+    )
+})
+locLines <- do.call(rbind, lineData)
+
+plot(loc1, xlim = xLim, ylim = yLim)
+# lines(locEdges)
+arrows(
+    c(NA, locEdges[,1]),
+    c(NA, locEdges[,2]),
+    c(locEdges[,1], NA),
+    c(locEdges[,2], NA),
+    length = 0.1
+)
+points(loc2, col='red')
+lines(locLines, col='red', lty=2)
+
+# dev.new()
+plot(g, layout=loc1)
+
+rn <- rownames(danube$data)
+colSums(rn %m==% t(unique(rn)))
