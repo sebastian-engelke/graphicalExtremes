a8a875b0d042f4589cf3d6476d697bffb12bc985
Sebastian Engelke <sengelke@gmx.de>
2024-03-21 17:44:36 +0100

    changes eglatent docu

--- R/eglatent.R
+++ R/eglatent.R
@@ -6,32 +6,43 @@
 #' fits an extremal graph structure with latent variables.
 #' 
 #' 
-#' @param Gamma Numeric matrix, ??
+#' @param Gamma conditionally negative semidefinite matrix. This will be typically the empirical variogram matrix.
 #' 
-#' @param lam1_list Numeric vector, ??
+#' @param lam1_list Numeric vector of non-negative regularization parameters for eglatent.
+#' Default is `lam1_list = c(0.1, 0.15, 0.19, 0.205)`.
 #' 
-#' @param lam2_list Numeric vector, ??
+#' @param lam2_list Numeric vector of non-negative regularization parameters for eglatent.
+#' Default is `lam2_list = c(2)`.
 #' 
-#' @param refit Logical scalar, ??
+#' @param refit Logical scalar, if TRUE then the model is refit on the estimated graph to obtain an estimate of the Gamma matrix on that graph.
+#' Default is `refit = TRUE`.
 #' 
 #' @param verbose Logical scalar, indicating whether to print progress updates.
 #' 
-#' @return List made of:
-#' \item{`G_obs`}{
-#'   List of numeric matrices, ??
-#' }
-#' \item{`G_obs_refit`}{
-#'   List of numeric matrices, ?? if `refit == TRUE` ??
-#' }
-#' \item{`graph`}{
-#'   List of [`igraph::graph`] objects, ??
+#' @return The function fits one model for each combination 
+#' of values in `lam1_list` and `lam2_list`. All returned objects 
+#' have one entry per model. List consisting of:
+#' #' \item{`graph`}{
+#'   A list of [`igraph::graph`] objects representing the
+#'   fitted graphs.
 #' }
 #' \item{`rk`}{
-#'   Numeric vector, ??
+#'   Numeric vector containing the estimated ranks of the latent
+#'   variables.
 #' }
-#' 
-#' @examples
-#' ??
+#' \item{`G_est`}{
+#'   A list of numeric estimated \dxd
+#'   variogram matrices \eGamma corresponding to the fitted graphs. 
+#' }
+#' \item{`G_refit`}{
+#'   A list of numeric estimated \dxd
+#'   variogram matrices \eGamma refitted with fixed graphs 
+#'   corresponding to the fitted graphs.
+#' }
+#' \item{`lambda_list`}{
+#'   A list containing the values of `lam1_list` and `lam2_list` 
+#'   used for the model fit.
+#' }
 #' 
 #' @references \insertAllCited{}
 #'
@@ -40,7 +51,7 @@
 eglatent <- function(
   Gamma,
   lam1_list = c(0.1, 0.15, 0.19, 0.205),
-  lam2_list = c(0.1, 0.15, 0.19, 0.205),
+  lam2_list = c(2),
   refit = TRUE,
   verbose = FALSE
 ){
@@ -54,11 +65,10 @@ eglatent <- function(
   d <- nrow(Gamma)
   U <- svd(diag(d) - 1 / d * matrix(1, d, 1) %*% t(matrix(1, d, 1)))$u[, 1:as.numeric(d - 1)]
   r <- 1
-  Gamma_obs <- list()
   graph_obs <- list()
   rk_vec <- numeric()
-  G_obs <- list()
-  G_obs_refit <- list()
+  G_est <- list()
+  G_refit <- list()
   lambda_list <- list()
 
   logcat('Iterating over lambda values...\n')
@@ -81,8 +91,7 @@ eglatent <- function(
       constraints <- list(P == S - L, U %*% t(U) %*% (P) %*% U %*% t(U) == P)
       prob <- CVXR::Problem(objective, constraints)
       result <- CVXR::solve(prob)
-      # G_obs[[r]] <- Theta2Gamma(result$getValue(P))
-      G_obs[[r]] <- graphicalExtremes:::ensure_matrix_symmetry(Theta2Gamma(result$getValue(P), check = FALSE))
+      G_est[[r]] <- graphicalExtremes:::ensure_matrix_symmetry(Theta2Gamma(result$getValue(P), check = FALSE))
 
       rk_vec[r] <- length(which(eigen(result$getValue(L))$values >= 10^(-3)))
       if (rk_vec[r] == 0) {
@@ -113,7 +122,7 @@ eglatent <- function(
           P <- CVXR::Variable(d, d, PSD = TRUE)
           M <- CVXR::Variable(rk, rk, PSD = TRUE)
           S <- CVXR::Variable(d, d, PSD = TRUE)
-          R <- -CVXR::log_det(t(U) %*% P %*% U) - 1 / 2 * sum(CVXR::diag(P %*% Gamma)) # Bitrate
+          R <- -CVXR::log_det(t(U) %*% P %*% U) - 1 / 2 * sum(CVXR::diag(P %*% Gamma)) 
           objective <- CVXR::Minimize(R)
           constraints <- list(P == S - subspace_est %*% M %*% t(subspace_est), U %*% t(U) %*% (P) %*% U %*% t(U) == P, A %*% reshape_expr(S, c(d^2, 1)) == 0)
 
@@ -122,7 +131,7 @@ eglatent <- function(
         } else {
           P <- CVXR::Variable(d, d, PSD = TRUE)
           S <- CVXR::Variable(d, d, PSD = TRUE)
-          R <- -CVXR::log_det(t(U) %*% P %*% U) - 1 / 2 * sum(CVXR::diag(P %*% Gamma)) # Bitrate
+          R <- -CVXR::log_det(t(U) %*% P %*% U) - 1 / 2 * sum(CVXR::diag(P %*% Gamma)) 
           objective <- CVXR::Minimize(R)
           constraints <- list(P == S, U %*% t(U) %*% (P) %*% U %*% t(U) == P, A %*% CVXR::reshape_expr(S, c(d^2, 1)) == 0)
 
@@ -130,16 +139,17 @@ eglatent <- function(
           result <- CVXR::solve(prob)
         }
 
-        G_obs_refit[[r]] <- ensure_matrix_symmetry(Theta2Gamma(result$getValue(P), check = FALSE))
+        G_refit[[r]] <- ensure_matrix_symmetry(Theta2Gamma(result$getValue(P), check = FALSE))
       }
       r <- r + 1
     }
   }
 
   return(list(
-    G_obs = G_obs,
-    G_obs_refit = G_obs_refit,
-    graph = graph_obs,
-    rk = rk_vec
+    graph = graph,
+    rk = rk_vec,
+    G_est = G_est,
+    G_refit = G_refit,
+    lambda_list = lambda_list
   ))
 }
